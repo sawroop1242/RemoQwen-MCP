53bec5d02a57c536875a6f73124bbac6b94c407d
sawroop1242 <swroopss487@gmail.com>
2026-04-30 18:38:48 +0530

    Use environment variable for server port

--- run.py
+++ run.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
         uvicorn.run(
             app, 
             host="0.0.0.0", 
-            port=8000, 
+            port=int(os.getenv("PORT", 8000)), 
             access_log=False, 
             log_level="critical",
             timeout_keep_alive=36000 # 10-Hour persistence layer
